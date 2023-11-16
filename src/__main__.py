87c9b21aca8404017424057f7abc3ab5d59fa24a
averagepythonfan <djonvorobei@bk.ru>
2023-11-16 18:08:17 +0300

    src main fix

--- src/__main__.py
+++ src/__main__.py
@@ -10,11 +10,10 @@ async def healthcheck():
     return {"text": "Back app is working now!"}
 
 
-if __name__ == "__main__":
-    uvicorn.run(
-        '__main__:app',
-        host="0.0.0.0",
-        port=9090,
-        log_level="info",
-        reload=True
-    )
+uvicorn.run(
+    '__main__:app',
+    host="0.0.0.0",
+    port=9090,
+    log_level="info",
+    reload=True
+)
