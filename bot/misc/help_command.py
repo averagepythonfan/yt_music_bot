5c445f5bba1cd105ac330bf21a04f9a0b45cbc03
averagepythonfan <djonvorobei@bk.ru>
2023-11-27 21:15:00 +0300

    help command update

--- bot/misc/help_command.py
+++ bot/misc/help_command.py
@@ -1,8 +1,17 @@
 help_message = """<b>Hello there!</b>
 
 This is DMKH bot that helps you download music from YouTube.
-As playlist and also as only one video.
+As playlist (unavailable now) and also as only one video.
+
+Commands:
+
+<b>/track</b> [YouTube vid link] - accept yt links LIKE:
+    - https://www.youtube.com/watch?v=<i>HASH</i>
+    - https://youtu.be/<i>HASH</i>
+
+<b>/status</b> - show user's status: <i>`GUEST`</i> - default.
 
 Also if you want to run your own instanse of bot,
 you can clone <b><a href="https://github.com/averagepythonfan/yt_music_bot">repository</a></b>
-"""
+
+Contact me directy: <b>@forgottenbb</b>"""
